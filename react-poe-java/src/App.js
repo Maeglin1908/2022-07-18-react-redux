561e982ff338464b139b798f16fd2ed565992c67
Nilgam <benjamin.thiery1908@gmail.com>
2022-07-18 15:19:58 +0200

    Added Routing

--- react-poe-java/src/App.js
+++ react-poe-java/src/App.js
@@ -1,15 +1,26 @@
-import Logo from './components/Logo';
-import Presentation from './pages/Presentation';
-import './styles/App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Logo from "./components/Logo";
+import Navigation from "./components/Navigation";
+import Presentation from "./pages/Presentation";
+import Routing from "./pages/Routing";
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <Logo />
-        <h1>React - POE Java</h1>
-      </header>
-      <Presentation />
+      <BrowserRouter>
+        <header>
+          <Logo />
+          <h1>React - POE Java</h1>
+        </header>
+        <Navigation />
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<Presentation />} />
+            <Route path="/routing" element={<Routing />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
