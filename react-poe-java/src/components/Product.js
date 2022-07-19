9d8f8ae3cb12d7afdb2254f90605a318f8d08979
Nilgam <nilgam@proton.me>
2022-07-19 15:30:08 +0200

    Adde PropTypes for validation

--- react-poe-java/src/components/Product.js
+++ react-poe-java/src/components/Product.js
@@ -1,8 +1,11 @@
-const Product = ({ prod }) => {
+import PropTypes from "prop-types";
+import "./../styles/Product.css";
+export const Product = ({ id, prod, currency = "€" }) => {
   //{prod} will get directly the props.prod into it.
 
   return (
     <div className="card">
+      <pre>From Product.js : {'const Product = ({ prod, currency = "€" })'}</pre>
       <pre>
         From Product.js : {"<h3>{prod.name}</h3> (ou props.prod si on ne le déstructure pas dans le paramètre)"}
       </pre>
@@ -11,12 +14,37 @@ const Product = ({ prod }) => {
       <article>
         <h4>Desc du produit</h4>
         <pre>{"<p>Nom : {prod.name}</p>"}</pre>
+        <pre>{"<p>Prix : {prod.prix} {currency}</p>"}</pre>
         <p>Nom : {prod.name}</p>
-        <pre>{"<p>Prix : {prod.prix} €</p>"}</pre>
-        <p>Prix : {prod.prix} €</p>
+        <p>
+          Prix : {prod.prix} {currency}
+        </p>
       </article>
     </div>
   );
 };
 
-export default Product;
+export const ProductTyped = ({ id, prod, currency = "€" }) => {
+  //{prod} will get directly the props.prod into it.
+
+  return (
+    <div className="card">
+      <h3>{prod.name}</h3>
+
+      <article>
+        <h4>Desc du produit</h4>
+        <p>Nom : {prod.name}</p>
+        <p>
+          Prix : {prod.prix} {currency}
+        </p>
+      </article>
+    </div>
+  );
+};
+
+ProductTyped.propTypes = {
+  id: PropTypes.string.isRequired,
+  prod: PropTypes.object.isRequired,
+  currency: PropTypes.oneOf(["€", "$", "CHF", "£"]),
+  same_as: PropTypes.arrayOf(PropTypes.string),
+};
