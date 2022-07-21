44a25dfd54644a07d12022d9a355487944f626a6
Nilgam <benjamin.thiery1908@gmail.com>
2022-07-21 15:25:16 +0200

    Using async await in useEffect for User Crud

---
+++ react-poe-java/src/components/UserCard.js
@@ -0,0 +1,13 @@
+import "./../styles/UserCard.css";
+
+const UserCard = ({ user: u }) => (
+    <div className="card">
+        <h4>
+            {u?.first_name} {u?.last_name}
+        </h4>
+        <p>{u?.gender}</p>
+        <p>{u?.email}</p>
+    </div>
+);
+
+export default UserCard;
