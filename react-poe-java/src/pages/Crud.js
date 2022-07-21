07c2b5c70220113d01a0350d820afc1314e99031
Nilgam <nilgam@proton.me>
2022-07-21 15:25:16 +0200

    Using async await in useEffect for User Crud

--- react-poe-java/src/pages/Crud.js
+++ react-poe-java/src/pages/Crud.js
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
+import UserCard from "../components/UserCard";
 import { getAll } from "../utils/services/UserService";
 
 const Crud = function () {
-    const [user, setUser] = useState(undefined);
+    const [users, setUser] = useState(undefined);
     useEffect(() => {
+        console.log("useEffect 1");
         const fetchUsers = async () => {
             const user_response = await getAll();
             setUser(user_response);
         };
+        fetchUsers();
     }, []);
 
     return (
@@ -16,6 +19,8 @@ const Crud = function () {
 
             <article>
                 <h2>All Users</h2>
+
+                <div className="grid">{users && users.map((u) => <UserCard key={u.id} user={u} />)}</div>
             </article>
         </>
     );
