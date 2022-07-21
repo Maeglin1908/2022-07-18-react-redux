cb5073629aed299c6c12dd1e6b5b4ac5f7149ba1
Nilgam <benjamin.thiery1908@gmail.com>
2022-07-21 15:50:54 +0200

    UserForm to add user to db.json with crud

--- react-poe-java/src/pages/Crud.js
+++ react-poe-java/src/pages/Crud.js
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import UserCard from "../components/UserCard";
+import { UserForm } from "../components/UserForm";
 import { getAll } from "../utils/services/UserService";
 
 const Crud = function () {
-    const [users, setUser] = useState(undefined);
+    const [users, setUser] = useState([]);
+
+    const [update, setUpdate] = useState(0);
     useEffect(() => {
         console.log("useEffect 1");
         const fetchUsers = async () => {
@@ -11,16 +14,27 @@ const Crud = function () {
             setUser(user_response);
         };
         fetchUsers();
-    }, []);
+    }, [update]);
 
     return (
         <>
             <h1>Users Dashboard</h1>
 
+            <p>
+                <i>
+                    Ne pas oublier de lancer <code>npm run api</code>
+                </i>
+            </p>
+            <article>
+                <UserForm reload={() => setUpdate(update + 1)} />
+            </article>
             <article>
                 <h2>All Users</h2>
-
-                <div className="grid">{users && users.map((u) => <UserCard key={u.id} user={u} />)}</div>
+                <div className="grid">
+                    {users.map((u) => (
+                        <UserCard key={u.id} user={u} />
+                    ))}
+                </div>
             </article>
         </>
     );
